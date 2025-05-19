18658a5829361dc8335a76912718477eaa3933db
MrBrightsid <stephenghughes@yahoo.co.uk>
2025-05-19 21:00:58 +0100

    initial commit

---
+++ src/backend_modules/ExpCRUD.js
@@ -0,0 +1,45 @@
+
+const API_BASE_URL = 'Example'
+export async function createExperiment(body){
+    const response = await fetch(`${API_BASE_URL}/get`,{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(body),
+    });
+}
+
+export async function updateExperiment(body){
+    const response = await fetch(`${API_BASE_URL}/get`,{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(body),
+    });
+    return response.json()
+}
+
+export async function readExperiment(body){
+    const response = await fetch(`${API_BASE_URL}/get`,{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(body),
+    });
+    return response.json()
+
+}
+
+export async function deleteExperiment(body){
+    const response = await fetch(`${API_BASE_URL}/get`,{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(body),
+    });
+    return response.json()
+}
