a5e1acf86b0ed4c18896287dd8f05708b4bf089e
DarrenBradleyy <darren.bradley.2022@uni.strath.ac.uk>
2025-05-20 00:39:33 +0100

    Create whole app shell

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import React from 'react';
-import './App.css';
+import ExperimentManagerUI from "./ExperimentManagerUI";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Experiment Manager</h1>
-      <form>
-        <label>
-          Bit Depth:
-          <select>
-            <option>8</option>
-            <option>10</option>
-          </select>
-        </label><br />
-        <label>
-          Spatial Resolution:
-          <input type="text" placeholder="e.g. 1920x1080" />
-        </label><br />
-        <label>
-          Temporal Resolution:
-          <select>
-            <option>Auto</option>
-            <option>30fps</option>
-            <option>60fps</option>
-          </select>
-        </label><br />
-        <label>
-          Duration:
-          <select>
-            <option>Auto</option>
-            <option>Short</option>
-            <option>Long</option>
-          </select>
-        </label><br />
-        <label>
-          Encoder Mode:
-          <select>
-            <option>Auto</option>
-            <option>CBR</option>
-            <option>VBR</option>
-          </select>
-        </label><br />
-        <fieldset>
-          <legend>Encoders</legend>
-          <label><input type="checkbox" /> AVC</label>
-          <label><input type="checkbox" /> HEVC</label>
-          <label><input type="checkbox" /> SHVC</label>
-          <label><input type="checkbox" /> VVC</label>
-          <label><input type="checkbox" /> SVC</label>
-        </fieldset><br />
-        <button type="submit">Run Experiment</button>
-      </form>
-    </div>
+      <div>
+        <ExperimentManagerUI />
+      </div>
   );
 }
 
