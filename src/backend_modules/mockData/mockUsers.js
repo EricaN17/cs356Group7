ba5d5210a48868db82552012a18cc1494035b125
EricaN17 <115188319+EricaN17@users.noreply.github.com>
2025-07-04 01:12:32 +0100

    fixing conflicts

--- src/backend_modules/mockData/mockUsers.js
+++ src/backend_modules/mockData/mockUsers.js
@@ -49,5 +49,5 @@ export function setMockToken(username) {
             .replace(/=+$/, '');
 
     const token = `${encode(header)}.${encode(payload)}.`;
-    window.localStorage.setItem('id_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlcmljYSIsInJvbGUiOiJzdXBlcl9hZG1pbiIsImZpcnN0X25hbWUiOiJzdHJpbmciLCJsYXN0X25hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZyIsImlhdCI6MTc1MTU4Mzk5NCwiZXhwIjoxNzUxNTkxMTk0fQ.uUxX80OIuSSvfrvlf34JGesY-fQT8St6QKprPWhAQQk');
+    window.localStorage.setItem('id_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInJvbGUiOiJzdXBlcl9hZG1pbiIsImZpcnN0X25hbWUiOiJzdHJpbmciLCJsYXN0X25hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZ3MiLCJpYXQiOjE3NTE1MDQ0NTEsImV4cCI6MTc1MTUxMTY1MX0.389tAR6NiYTiFUWSEDwx6WmYadj6zqZwPLw6sR2QvSg');
 }
